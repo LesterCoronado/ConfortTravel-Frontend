18b4bbdcd6e27e7ea8858c1f05dcd94cbf63998b
LesterCoronado <lestercoronado730@gmail.com>
2024-10-17 18:03:55 -0600

    console.log("hurray")

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { BackendService } from '../services/backend.service';
+import { environment } from '../environments/environments.prod';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -13,7 +15,16 @@ import { RippleModule } from 'primeng/ripple';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService, private backend: BackendService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.backend.get(`${environment.api}/Empleado`).subscribe({
+      next: (data: any) => {
+        console.log("hurray");
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
