44d05913118be21df8cfc94ca7412904126454ce
LesterCoronado <lestercoronado730@gmail.com>
2024-10-19 08:29:14 -0600

    new changes

--- src/app/environments/environments.prod.ts
+++ src/app/environments/environments.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     // api:'https://localhost:7096/api',
-    api:'https://confortapp-e6buffa7e0gshveq.centralus-01.azurewebsites.net/api',
+    api:'https://backendapp-g5grgdfba3hwb0c9.centralus-01.azurewebsites.net/api',
     X_PUBLIC_KEY: 'pk_live_EDFK8waezN0DnOue1Mtw4idvI2r09RPGZNXy8EUpfi73zpjVweqbyzpOm',
     X_SECRET_KEY: 'sk_live_WHCmxsEMmFzskKCFXtt6FXZkrnb8umtaLjMNnoQXCkFr7yqc5ahkxEsCW',
   
